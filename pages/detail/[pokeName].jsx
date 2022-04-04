40cdd85db50fc70ed2cf3994c446824557d1c62f
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-04 19:33:54 +0700

    fixing error when load localStorage
    hasn't complete the feature to delete localStorage, provide a nickname

--- pages/detail/[pokeName].jsx
+++ pages/detail/[pokeName].jsx
@@ -1,14 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import React from 'react';
-import {
-	CardBox,
-	CardMoves,
-	CardStats,
-	Pokeball,
-} from '../../components/Cards';
+import Swal from 'sweetalert2';
+import { CardBox, CardMoves, CardStats } from '../../components/Cards';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import MyLink from '../../components/Link';
 import Navbar from '../../components/Navbar';
 
 export async function getServerSideProps(context) {
@@ -28,7 +25,45 @@ export async function getServerSideProps(context) {
 	};
 }
 
-const PokeDetail = ({ pokemon, types, stats, abilities, moves }) => {
+const PokeDetail = ({ pokemon, types, stats, moves }) => {
+	const pushLocalStorage = () => {
+		let getLocal = JSON.parse(localStorage.getItem('ownedPokemon'));
+		let ownedPokemon = {
+			pokemon: pokemon.name,
+			type: types.map((item) => item.type.name).join(', '),
+			key: pokemon.id,
+		};
+		if (getLocal) {
+			getLocal.push(ownedPokemon);
+			localStorage.setItem('ownedPokemon', JSON.stringify(getLocal));
+		} else {
+			localStorage.setItem(
+				'ownedPokemon',
+				JSON.stringify([ownedPokemon])
+			);
+		}
+	};
+
+	function Gacha() {
+		const random = Math.floor(Math.random() * 100);
+		if (random <= 50) {
+			Swal.fire({
+				title: 'Congrats!',
+				text: 'You got a pokemon!',
+				icon: 'success',
+				confirmButtonText: 'Hooray!',
+			});
+			pushLocalStorage();
+		} else {
+			Swal.fire({
+				title: 'Oops...',
+				text: 'You missed!',
+				icon: 'error',
+				confirmButtonText: 'Cool',
+			});
+		}
+	}
+
 	return (
 		<>
 			<Head>
@@ -83,7 +118,27 @@ const PokeDetail = ({ pokemon, types, stats, abilities, moves }) => {
 				/>
 			</div>
 
-			<Pokeball />
+			<div className='text-center my-5'>
+				<a href='#'>
+					<Image
+						src='/pokeball.svg'
+						alt='Pokeball'
+						width={80}
+						height={80}
+						onClick={() => {
+							Gacha();
+						}}
+					/>
+					<p
+						className='rounded-pill border mx-auto w-50 my-4 text-white fw-bold'
+						onClick={() => {
+							Gacha();
+						}}
+						style={{ backgroundColor: '#F25430' }}>
+						Catch <br /> Pokemon
+					</p>
+				</a>
+			</div>
 
 			<Footer />
 		</>
